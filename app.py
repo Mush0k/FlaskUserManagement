db3de0a5509c5523bd6d5cadbcc5969a875b0993
Mush0k <Mushoki@yandex.ru>
2025-12-11 11:30:43 +0400

    done?

--- app.py
+++ app.py
@@ -1,14 +1,87 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
+import mysql.connector
+from mysql.connector import errorcode
+
+# 1. инициализация Flask и настройки БД
+
+# создание экземпляра Flask-приложения
 app = Flask(__name__)
+
+# настройки подключения к БД 
+DB_CONFIG = {
+    'host': 'localhost',
+    'database': 'mushok',
+    'user': 'mewo',
+    'password': 'mushok229mewo' 
+}
+
+# 2. проверка авторизации в БД 
+
+def check_user_credentials(username, password):
+  #проверка данных 
+    cnx = None
+    try:
+        cnx = mysql.connector.connect(**DB_CONFIG)
+        cursor = cnx.cursor()
+
+        # SQL-запрос для поиска пользователя
+        #ПОМЕТКА: в моей бд используются
+        #хэш-затыки паролей (например "hash1"), поэтому 
+        # я использую их вместо самого пароля. в идеале конечно же
+        #чтоб там был реальный хэш пароля и происходило какое-то сравнение(?)
+        query = "SELECT user_id FROM users WHERE username = %s AND password_hash = %s"
+        cursor.execute(query, (username, password))
+        
+        # если найдена хотя бы одна строка, возвращаем True (авторизация успешна)
+        return cursor.fetchone() is not None
+
+    except mysql.connector.Error as err:
+        # вывод ошибки подключения БД в консоль (если БД выключена, логин неверный и т.п.)
+        print(f"Ошибка БД при проверке: {err}")
+        return False
+    finally:
+        if cnx and cnx.is_connected():
+            cnx.close()
+
+
+
+# 3. роуты
+
+# роут для главной страницы (Пункт 4)
 @app.route('/')
 def home():
-    return "<h1>мяумяумяу</h1>" #простой роут
+    # возвращаем простой роут, который открывается в браузере
+    return "<h1>Сервер работает!</h1><p>Необходимо перейти на <a href='/login'>/login</a> для авторизации.</p>"
 
-# роут для отображения страницы авторизации 
-@app.route('/login')
+
+# роут для авторизации 
+@app.route('/login', methods=['GET', 'POST']) #GET - показать форму, POST - принять данные
 def login_page():
-    # render_template ищет файл в папке 'templates'
-    return render_template('login.html')
+    message = None # Переменная для сообщения пользователю
+
+    if request.method == 'POST':
+        #принятие данных из фоурмы
+        username = request.form['username']
+        password = request.form['password']
+        
+        # вывод принятых данных в консоль 
+        print(f"\n   Полученные данные   ")
+        print(f"Логин: {username}")
+        print(f"Пароль: {password}")
+        print(f"----------------------------------\n")
+        
+ #обработка формы
+        if check_user_credentials(username, password):
+            message = "Авторизация прошла успешно!"
+        else:
+            message = "Ошибка: Неверный логин или пароль."
+            
+    # отображение шаблона login.html и передача ему сообщния
+    return render_template('login.html', message=message)
+
+
+# 4. точка входа для запуска
 
 if __name__ == '__main__':
+    # запуск сервера
     app.run(debug=True)
